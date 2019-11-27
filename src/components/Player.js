2b6537feefc5fa193e4ec806656b6a4c48698b40
Brandon <brandon.lau86@gmail.com>
2019-11-27 00:24:42 -0500

    Remove spotifyplayer npm

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import SpotifyPlayer from 'react-spotify-web-playback';
 export default class Player extends Component {
   render() {
     return (
       <div>
-        <SpotifyPlayer token = {this.props.accessToken} uris = "spotify:track:44Ljlpy44mHvLJxcYUvTK0"/>
       </div>
     )
   }
